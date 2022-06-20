8139e11fe0927834807d6dd8a14e9181e65eccd8
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-20 13:56:44 +0100

    changed title of .create_account test to be more accurate

--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -2,7 +2,7 @@ require 'account'
 
 describe "Account Class" do
   describe ".create_account" do
-    it "initialises a new account with a name and balance" do
+    it "returns an instance of account" do
       expect(Account.create_account).to be_an_instance_of(Account)
     end
   end

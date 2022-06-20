bdf69e66e41152e3a6cbc2b3edbd135f9a5f576c
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-20 14:15:04 +0100

    refactored to make name a keyword argument in Account initialisation

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,12 @@
 class Account 
   attr_reader :name, :balance
 
-  def initialize(name)
+  def initialize(name:)
     @name = name
     @balance = 0
   end
  
   def self.create_account(name)
-    return Account.new(name)
+    return Account.new(name: name)
   end
 end

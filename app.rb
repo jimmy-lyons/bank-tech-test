87b0608b714d8bb4e6b0393a8241d28d7e67e75b
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-21 14:11:02 +0100

    updated app to include withrawl

--- app.rb
+++ app.rb
@@ -14,4 +14,9 @@ class Bank
     Formatter.deposit_confirmation(value, account.balance)
   end
 
+  def self.make_withdrawl(id, value)
+    account = Account.withdraw(id: id, value: value)
+    Formatter.withdrawl_confirmation(value, account.balance)
+  end
+
 end

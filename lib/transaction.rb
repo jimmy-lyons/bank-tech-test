92544515bc5ff66de7b095c85271cf3ae60b562f
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-21 15:22:22 +0100

    refactored record so that information is stored in database

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -31,12 +31,18 @@ class Transaction
   end
 
   def self.record(user_id:, credit:, debit:, balance:)
+    result = DatabaseConnection.query(
+      "INSERT INTO transactions (transaction_date, user_id, credit, debit, balance) 
+      VALUES($1, $2, $3, $4, $5) RETURNING transaction_id, transaction_date, user_id, credit, debit, balance;",
+      [Time.now.strftime("%d/%m/%Y %H:%M"), user_id, credit, debit, balance]
+    )
+
     Transaction.new(
-      user_id: user_id, 
-      credit: credit, 
-      debit: debit, 
-      balance: balance,
-      transaction_date: Time.now.strftime("%d/%m/%Y %H:%M")
+      user_id: (result[0]['user_id']).to_i, 
+      credit: (result[0]['credit']).to_f, 
+      debit: (result[0]['debit']).to_f, 
+      balance: (result[0]['balance']).to_f,
+      transaction_date: result[0]['transaction_date']
     )
   end
 end

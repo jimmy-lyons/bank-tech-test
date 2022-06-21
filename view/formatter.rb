0ba15dca251eeeabbdb17f06f44add9867516013
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-21 11:04:44 +0100

    new account signup message added

--- view/formatter.rb
+++ view/formatter.rb
@@ -1,7 +1,12 @@
 class Formatter
   
-  def self.deposit_confirmation(value, new_balance)
+  def self.new_account_confirmation(name, id)
+    statement = "Thankyou #{name} for joining Bank, your account number is #{id}"
+    p statement
+    return statement
+  end
 
+  def self.deposit_confirmation(value, new_balance)
     statement = "Credit: £#{sprintf('%.2f', value)}, New Balance: £#{sprintf('%.2f', new_balance)}"
     p statement
     return statement

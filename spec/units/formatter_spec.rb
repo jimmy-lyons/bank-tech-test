dfa619df13dd84936d3fce759358f7a2b693a754
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-21 10:50:28 +0100

    added further tests for deposit statement

--- spec/units/formatter_spec.rb
+++ spec/units/formatter_spec.rb
@@ -6,5 +6,15 @@ describe 'Formatter Class' do
       expect(Formatter.deposit_confirmation(100.00, 200.00))
       .to eq("Credit: £100.00, New Balance: £200.00")
     end
+    
+    it 'it displays the deposited value and the new balance' do
+      expect(Formatter.deposit_confirmation(100.2, 200.2))
+      .to eq("Credit: £100.20, New Balance: £200.20")
+    end
+
+    it 'it displays the deposited value and the new balance' do
+      expect(Formatter.deposit_confirmation(100, 200))
+      .to eq("Credit: £100.00, New Balance: £200.00")
+    end
   end
 end

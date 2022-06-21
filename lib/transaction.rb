235cf02bfe35aa5734f52d331fcce4571c791667
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-21 14:36:29 +0100

    added record method to Transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,5 +1,23 @@
 require './database_connection_setup'
 
 class Transaction
+  def user_id
+    1
+  end
 
+  def credit
+    123.45
+  end
+
+  def debit
+    0
+  end
+
+  def balance
+    223.45
+  end
+
+  def self.record(user_id:, credit:, debit:, balance:)
+    Transaction.new
+  end
 end
--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -5,4 +5,19 @@ describe 'Transaction Class' do
     transaction = Transaction.new
     expect(transaction).to be_an_instance_of Transaction
   end
+
+  describe '.record' do
+    it 'records and returns values of a transaction' do
+      new_transaction = Transaction.record(
+        user_id: 1, 
+        credit: 123.45, 
+        debit: 0, 
+        balance: 223.45
+      )
+      expect(new_transaction.user_id).to eq 1
+      expect(new_transaction.credit).to eq 123.45
+      expect(new_transaction.debit).to eq 0
+      expect(new_transaction.balance).to eq 223.45
+    end
+  end
 end

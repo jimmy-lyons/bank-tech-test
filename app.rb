79495080f85e1b0e19d15e4db8ff0994c26c887c
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-21 12:48:59 +0100

    make_deposit added to app.rb

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require './database_connection_setup'
-require 'account'
-require 'formatter'
+require './lib/account'
+require './view/formatter'
 
 class Bank
 
-  def create_bank_account(name)
+  def self.create_bank_account(name)
     account = Account.create_account(name)
     Formatter.new_account_confirmation(account.name, account.id)
+  end
+
+  def self.make_deposit(id, value)
+    account = Account.deposit(id: id, value: value)
+    Formatter.deposit_confirmation(value, account.balance)
   end
 
 end
